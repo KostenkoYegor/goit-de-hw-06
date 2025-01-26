674c5ba0135e8cf97cc1ef9ff8d7fd130edbcfb1
KostenkoYegor <kostenkoegor1@gmail.com>
2025-01-26 13:44:48 +0100

    test.1.4

--- kafka_producer.py
+++ kafka_producer.py
@@ -5,6 +5,7 @@ import time
 import random
 
 def generate_sensor_data():
+
     return {
         "id": random.randint(1, 100),
         "temperature": random.uniform(18, 35),
@@ -13,18 +14,32 @@ def generate_sensor_data():
     }
 
 def produce_sensor_data():
-    producer = create_producer()
-    topic = 'yk_building_sensors'
-    
+    """Отправка данных в Kafka топик."""
+    producer = None
     try:
+        producer = create_producer()
+        print("Producer connected successfully.")
+        
+        topic = 'yk_building_sensors'
+        
         while True:
             data = generate_sensor_data()
-            producer.send(topic, value=data)
-            print(f"Sent data: {data}")
+            try:
+                producer.send(topic, value=data)
+                print(f"Sent data: {data}")
+            except Exception as e:
+                print(f"Error while sending data: {e}")
             time.sleep(1)
+    
     except KeyboardInterrupt:
         print("Producer stopped.")
+    except Exception as e:
+        # Обработка ошибок при создании producer или отправке данных
+        print(f"Error while creating producer or sending data: {e}")
     finally:
-        producer.close()
+        # Закрытие producer
+        if producer:
+            producer.close()
+            print("Producer closed.")
 
 produce_sensor_data()
