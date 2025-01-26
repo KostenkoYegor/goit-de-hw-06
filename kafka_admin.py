9ab361b3280606018537128ac1c012cf383dbc0f
KostenkoYegor <kostenkoegor1@gmail.com>
2025-01-26 13:54:41 +0100

    test.1.5

--- kafka_admin.py
+++ kafka_admin.py
@@ -1,26 +1,31 @@
 # kafka_admin.py
 
-from kafka_config import create_admin_client, NewTopic
+from kafka_config import create_admin_client
+from kafka.admin import NewTopic
 
 def create_topics():
     admin_client = create_admin_client()
-    
+
     topics = [
         NewTopic(name="yk_building_sensors", num_partitions=1, replication_factor=1),
         NewTopic(name="yk_temperature_alerts", num_partitions=1, replication_factor=1),
         NewTopic(name="yk_humidity_alerts", num_partitions=1, replication_factor=1),
     ]
-    
+
     try:
-        existing_topics = admin_client.list_topics()
+
+        existing_topics = set(admin_client.list_topics())
         topics_to_create = [topic for topic in topics if topic.name not in existing_topics]
-        
+
         if topics_to_create:
             admin_client.create_topics(new_topics=topics_to_create, validate_only=False)
-            print("Topics created successfully.")
+            print(f"Created topics: {[topic.name for topic in topics_to_create]}")
         else:
-            print("Topics already exist.")
+            print("All topics already exist.")
     except Exception as e:
         print(f"Error creating topics: {e}")
     finally:
         admin_client.close()
+
+if __name__ == "__main__":
+    create_topics()
